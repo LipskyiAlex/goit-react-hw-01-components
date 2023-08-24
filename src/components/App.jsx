8fe20c5b0fa03c11ab9a91e3e121a7e2589868c8
Alex <alexlipskyi@icloud.com>
2023-08-24 19:22:06 +0300

    test2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Profile from "./Profile/Profile";
-import Statistics from "./Statistics/Statistics";
-import FriendList from "./FriendList/FriendList";
-import TransactionHistory from './TransactionHistory/TransactionHistory';
+import Profile from "./Profile/Profile.jsx";
+import Statistics from "./Statistics/Statistics.jsx";
+import FriendList from "./FriendList/FriendList.jsx";
+import TransactionHistory from './TransactionHistory/TransactionHistory.jsx';
 
 import user from "../data/user.json";
 import data from "../data/data.json";
